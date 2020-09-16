6eb003bd34274dea5bb5eab7a574cfb9254c64d8
AnishChatti <177r1a05k1@gmail.com>
2020-09-16 21:16:21 +0530

    INITIAL-COMMIT

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.static(`${__dirname}/public`));
+
+app.listen(port, () => {
+ console.log('Server is up on port ' + port)
+})
